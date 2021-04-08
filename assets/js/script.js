5551f6c3d0de17b14e5ad1a0ca0eb0c1f7207a62
hellyryan1995 <helly.ryan1995@gmail.com>
2021-04-08 12:38:27 +0000

    Added remove start button and display random commands

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,15 +1,42 @@
 
+let commands = ["Red", "Yellow", "Blue", "Green"];
+let displayTime = document.querySelector("#timer");
+let score = 0;
+let command = randomCommand();
+
+// Removes Start button when the game begins
+function removeButton(){
+    $( "#startButton" ).slice( 0 ).css( "display", "none" );
+}
+
+// Will create one of the 4 word randomly
+function randomCommand(){
+    return commands[Math.floor(Math.random() * commands.length)];
+}
+
+// display random command word
+function displayCommand(command) {
+    if (command === "Red"){
+        $("h2").removeClass("red-command");
+    }
+    if (command === "Yellow") {
+        $("h2").removeClass("yellow-command");
+    }
+    if (command === "Blue") {
+        $("h2").removeClass("blue-command");
+    }
+    if (command === "Green") {
+        $("h2").removeClass("green-command");
+    }
+}
+
 function startGame() {
+    removeButton();
+    score = 0;
+    displayCommand(command);
 
 }
 
 function playGame(){
     
 }
-
-function displayCommands() {
-
-}
-
-
-
