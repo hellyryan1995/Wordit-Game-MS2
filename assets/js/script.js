1001a9ba91df547623ca549c8c9627cc0528e234
hellyryan1995 <helly.ryan1995@gmail.com>
2021-04-15 23:06:29 +0000

    Fixed game loop within playGame function and cleaned up commands code

--- assets/js/script.js
+++ assets/js/script.js
@@ -13,17 +13,20 @@ function randomCommand(){
 
 // display random command word
 function displayCommand(command) {
-    if (command === "Red"){
-        $("h2").removeClass("red-command");
+    console.log("Show random word");
+    document.getElementById("command").textContent = command
+
+     if (command === "Red"){
+        $("h2").removeClass("command");
     }
-    if (command === "Yellow") {
-        $("h2").removeClass("yellow-command");
+     if (command === "Yellow"){
+        $("h2").removeClass("command");
     }
-    if (command === "Blue") {
-        $("h2").removeClass("blue-command");
+     if (command === "Blue"){
+        $("h2").removeClass("command");
     }
-    if (command === "Green") {
-        $("h2").removeClass("green-command");
+    if (command === "Green"){
+        $("h2").removeClass("command");
     }
 }
 
@@ -38,85 +41,73 @@ function startGame() {
 
 function playGame() {
 
-    // enables the game buttons
+    // Once game has started this will display the commands
+    let command = randomCommand();
+    displayCommand(command);
+
     document.getElementById("red-button").disabled = false;
     document.getElementById("yellow-button").disabled = false;
     document.getElementById("blue-button").disabled = false;
     document.getElementById("green-button").disabled = false;
 
-    //Help From w3school // This is used to insure the correct button is selected.
-    let redBtn = document.getElementById("red-button").getAttribute("data-type");
-    let yellowBtn = document.getElementById("yellow-button").getAttribute("data-type");
-    let greenBtn = document.getElementById("green-button").getAttribute("data-type");
-    let blueBtn = document.getElementById("blue-button").getAttribute("data-type");
-    $( "#red-command-styling" ).addClass("red-command");
-    $( "#yellow-command-styling" ).addClass("yellow-command");
-    $( "#green-command-styling" ).addClass("green-command");
-    $( "#blue-command-styling" ).addClass("blue-command");
-    
-    // Once game has started this will display the commands
-    let command = randomCommand();
-    displayCommand(command);
-    
     // Once the buttons are clicked and correct the game will continue if not then the game will end
-    $("#red-button").click(function() {
-        if (command === redBtn) {
+    document.getElementById("red-button").onclick = function() {
+        if (command === 'Red') {
             updateScore();
-            $( "#red-command-styling" ).addClass("red-command");
             playGame();
         } else {
             endGame();
+            $('#myModalOver').modal('show');
         }   
-    });
+    };
 
-    $("#yellow-button").click(function(){
-        if (command === yellowBtn) {
+    document.getElementById("yellow-button").onclick = function(){
+        if (command === 'Yellow') {
             updateScore();
-            $( "#yellow-command-styling" ).addClass("yellow-command");
             playGame();
         } else {
             endGame();
+            $('#myModalOver').modal('show');
         }   
-    });
+    };
 
-    $("#green-button").click(function(){
-        if (command === greenBtn) {
+    document.getElementById("green-button").onclick = function(){
+        if (command === 'Green') {
             updateScore();
-            $( "#green-command-styling" ).addClass("green-command");
             playGame();
         } else {
             endGame();
+            $('#myModalOver').modal('show');
         }   
-    });
+    };
 
-    $("#blue-button").click(function(){
-        if (command === blueBtn) {
+    document.getElementById("blue-button").onclick = function(){
+        if (command === 'Blue') {
             updateScore();
-            $( "#blue-command-styling" ).addClass("blue-command");
             playGame();
         } else {
             endGame();
+            $('#myModalOver').modal('show');
         }   
-    });
-
+    };
 }
 
 function endGame() {
 
     endGame.called = true;
 
-    //alert("Game over! You Lost!");
+    $('#myModalOver').modal('show');
+
     $("#startButton").show();
-    //disables the game buttons
+ 
 
     // Hide Timer
     $("#timer").hide();
 
-    $( "#red-command-styling" ).addClass("red-command");
-    $( "#yellow-command-styling" ).addClass("yellow-command");
-    $( "#green-command-styling" ).addClass("green-command");
-    $( "#blue-command-styling" ).addClass("blue-command");
+    //Removes Commands
+    $( "#command" ).addClass("command");
 
+    //disables the game buttons
     document.getElementById("red-button").disabled = true;
     document.getElementById("yellow-button").disabled = true;
     document.getElementById("blue-button").disabled = true;
@@ -129,8 +120,8 @@ const startBtn = document.querySelector("#startButton");
 let timeLeft = 3;
 
 function displayTimer() {
-    // Help From Code with Ania Kubów yon youtube
-    setInterval(function(){
+    // Help From Code with Ania Kubów on youtube
+    /*setInterval(function(){
         if(timeLeft <= 0 ) {
             endGame();
             clearInterval(timeLeft = 0);
@@ -140,14 +131,13 @@ function displayTimer() {
         timeLeft -=1;
         }, 1000)
 
-    startBtn.addEventListener("click", displayTimer);
-    }
+    startBtn.addEventListener("click", displayTimer);*/
+}
 
 // this will update the score once you click the correct answer
 function updateScore() {
     score++;
-  $("#score")[0].innerHTML = score;
-
+    $("#score")[0].innerHTML = score;
 }
 
 // This will reset the score back to 0 once the game starts again
